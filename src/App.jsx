8f6434e4363499ff0d11634b28eaa21808d5e06f
dahcodingkid <dahcodingkid@gmail.com>
2022-07-07 10:46:54 -0700

    loading fx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import ImageCard from './components/ImageCard'
+import LoadingFX from './components/LoadingFX'
 import SearchForm from './components/SearchForm'
 
 const App = () => {
   const [data, setData] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
+  const [loading, setLoading] = useState(true)
   const api_key = '28487137-191c4754f0cd9e0c364ce14aa'
 
   const onSubmit = (e) => {
@@ -13,12 +15,14 @@ const App = () => {
   }
 
   useEffect(() => {
+    setLoading(true)
     fetch(
       `https://pixabay.com/api/?key=${api_key}&q=${searchTerm}&image_type=photo`,
     )
       .then((res) => res.json())
       .then((res) => {
         setData(res.hits)
+        setLoading(false)
       })
       .catch((err) => console.log(err))
   }, [searchTerm])
@@ -38,13 +42,13 @@ const App = () => {
         />
 
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-          {data.map((image) => {
-            return data.length > 0 ? (
-              <ImageCard key={image.id} image={image} />
+          {data.map((image) =>
+            loading ? (
+              <LoadingFX />
             ) : (
-              <div className="text-2xl text-slate-100">No image found</div>
-            )
-          })}
+              <ImageCard key={image.id} image={image} />
+            ),
+          )}
         </ul>
         <div className="mx-auto text-xs text-slate-200 text-center">
           Made with <span>❤</span> by Trapcy
